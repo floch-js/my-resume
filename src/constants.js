317e4603c7fffa0f99c4bbb1752432c8bf6fbed3
7.mandrill <7.mandrill@7mandrills-MacBook-Pro.local>
2022-01-28 21:20:13 +0100

    Init

---
+++ src/constants.js
@@ -0,0 +1,75 @@
+export const LANGS = {
+  en: 'English',
+  fr: 'Français',
+  ko: '한국인',
+};
+
+export const SECTIONS = {
+  landing: 'landing',
+  purpose: 'purpose',
+  timeline: 'timeline',
+  skills: 'skills',
+  contacts: 'contacts',
+};
+
+export const EXPERIENCE_TYPES = {
+  education: 'education',
+  employee: 'employee',
+  freelance: 'freelance',
+};
+
+export const SKILLS = {
+  fr: 'fr',
+  en: 'en',
+  it: 'it',
+  html: 'html',
+  css: 'css',
+  js: 'js',
+  typescript: 'typescript',
+  react: 'react',
+  redux: 'redux',
+  reduxSaga: 'redux-saga',
+  formik: 'formik',
+  rff: 'rff',
+  reactQuery: 'react-query',
+  mui: 'mui',
+  bootstrap: 'bootstrap',
+  styledComp: 'styled-comp',
+  emotion: 'emotion',
+  tailwind: 'tailwind',
+  node: 'node',
+  express: 'express',
+  mongo: 'mongo',
+  webpack: 'webpack',
+  jwt: 'jwt',
+  keycloak: 'keycloak',
+  puppeteer: 'puppeteer',
+  algolia: 'algolia',
+  docker: 'docker',
+  jenkins: 'jenkins',
+  graphql: 'graphql',
+  ramda: 'ramda',
+  lodash: 'lodash',
+  jest: 'jest',
+  gatsby: 'gatsby',
+  next: 'next',
+  unix: 'unix',
+  git: 'git',
+  vim: 'vim',
+  googleAnalytics: 'google-analytics',
+  c: 'c',
+  cpp: 'cpp',
+  python: 'python',
+  d3: 'd3',
+  reactRouter: 'react-router',
+  grunt: 'grunt',
+  gulp: 'gulp',
+};
+
+export const SKILL_TYPES = {
+  language: 'language',
+  back: 'back-end',
+  front: 'front-end',
+  others: 'others',
+  tools: 'tools',
+};
---
+++ src/data/contacts/index.js
@@ -0,0 +1,7 @@
+export default {
+  fullName: 'FLOCH JS',
+  email: 'mail',
+  phone: '+tel',
+  github: 'flochjs',
+  githubLink: 'github-link',
+};
